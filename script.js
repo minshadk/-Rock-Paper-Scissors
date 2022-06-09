8f8cf8d0adfb9afe52f0a7deabd408908596bf73
minshadk <minshadk13@gmail.com>
2022-06-09 23:51:00 +0530

    desktop logic finshed

--- script.js
+++ script.js
@@ -8,52 +8,79 @@ const imagePath = {
 
 const resultBorder = {
   paper: "paper-result-border",
-  scissors: "scissors-result-border",
+  scissor: "scissor-result-border",
   rock: "rock-result-border",
 };
 
 const picker = document.querySelector(".picker");
 const result = document.querySelector(".result");
 const resultText = document.querySelector(".result-text");
+const scoreDigit = document.querySelector(".score-digit");
+
 const userChoiceImage = document.querySelector(".user-choice-image");
-console.log(userChoiceImage);
+const userChoiceRadius = document.querySelector(".user");
+
+const houseChoiceImage = document.querySelector(".house-choice-image");
+const houseChoiceRadius = document.querySelector(".house");
 
 function userChoice(userChoosed) {
   picker.style.display = "none";
   result.style.display = "flex";
+
+  // seting image src dynamicaly
   userChoiceImage.src = imagePath[userChoosed];
-  console.log(userChoosed);
-  console.log(userChoiceImage);
+  //   adding border class dynamicaly
+  userChoiceRadius.classList.add(resultBorder[userChoosed]);
   houseChoice(userChoosed);
 }
 
 function houseChoice(userChoosed) {
   const choices = ["paper", "scissor", "rock"];
   houseChoosed = choices[Math.floor(Math.random() * choices.length)];
-  console.log(houseChoosed);
+  // seting image src dynamicaly
+  houseChoiceImage.src = imagePath[houseChoosed];
+  //   adding border class dynamicaly
+  houseChoiceRadius.classList.add(resultBorder[houseChoosed]);
   findWinner(userChoosed, houseChoosed);
 }
 
 function findWinner(userChoosed, houseChoosed) {
   //  user wining
-  if (userChoosed === "paper" && houseChoosed === "rock")
-    console.log("YOU WIN");
-  else if (userChoosed === "scissor" && houseChoosed === "paper")
-    console.log("YOU WIN");
-  else if (userChoosed === "rock" && houseChoosed === "scissor")
-    console.log("YOU WIN");
+  if (userChoosed === "paper" && houseChoosed === "rock") {
+    setResult("YOU WIN");
+    setScore();
+  } else if (userChoosed === "scissor" && houseChoosed === "paper") {
+    setResult("YOU WIN");
+    setScore();
+  } else if (userChoosed === "rock" && houseChoosed === "scissor") {
+    setResult("YOU WIN");
+    setScore();
+  }
   // draw
   else if (userChoosed === "paper" && houseChoosed === "paper")
-    console.log("DRAW");
+    setResult("DRAW");
   else if (userChoosed === "scissor" && houseChoosed === "scissor")
-    console.log("DRAW");
-  else if (userChoosed === "rock" && houseChoosed === "rock")
-    console.log("DRAW");
+    setResult("DRAW");
+  else if (userChoosed === "rock" && houseChoosed === "rock") setResult("DRAW");
   // house winning
   else if (userChoosed === "paper" && houseChoosed === "scissor")
-    console.log("HOUSE WINS");
+    setResult("HOUSE WINS");
   else if (userChoosed === "scissor" && houseChoosed === "rock")
-    console.log("HOUSE WINS");
+    setResult("HOUSE WINS");
   else if (userChoosed === "rock" && houseChoosed === "paper")
-    console.log("HOUSE WINS");
+    setResult("HOUSE WINS");
+}
+
+function setResult(result) {
+  resultText.innerHTML = result;
+}
+
+function setScore() {
+  ++score;
+  scoreDigit.innerHTML = score;
+}
+
+function playAgain() {
+  picker.style.display = "flex";
+  result.style.display = "none";
 }
